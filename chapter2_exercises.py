fa6f60cd9e97c9675dda1676ca7373987c28ba2a
jinyoungan <jinny117@gmail.com>
2014-06-04 20:22:07 -0400

    the first week

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,21 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: JIN YOUN AN
+
+#2.1 Python interprets as octal if a number starts with zero
+
+
+#2.2 Nothing
+print 5
+x = 5
+print x + 1
+
+#2.3.1	8
+#2.3.2	8.5
+#2.3.3	4.0
+#2.3.4	11
+#2.3.5	.....
+
+#2.4.1 	523.33
+#2.4.2	$945.45
+#2.4.3	7:30:06 am
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,42 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: JIN YOUNG AN
+
+#3.1 NameError: name 'repeat_lyrics' is not defined
+
+#3.2 SyntaxError: invalid syntax
+
+#3.3
+def right_justify(s):
+	print " " * (70 - len(s)) + s
+right_justify('allen')
+
+#3.4.1
+def do_twice(f):
+	f()
+	f()
+
+def print_spam():
+	print 'spam'
+
+do_twice(print_spam)
+
+#3.4.2
+def do_twice(f, ham):
+	f(ham)
+	f(ham)
+
+#3.4.3
+def print_twice(ham):
+	print ham
+	print ham
+
+#3.4.4
+do_twice(print_twice, 'spam')
+
+#3.4.5
+def do_four(a, meat):
+	a(meat)
+	a(meat)
+
+do_four(print_twice, 'fish')
